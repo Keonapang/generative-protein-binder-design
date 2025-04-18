e3b88701dca81fcc811e99d8da606f46ad7fe2f8
Keonapang <keonapang2000@gmail.com>
2025-04-18 10:30:36 -0400

    write Apob-100 peptide binder design script

---
+++ src/RUN_protein_binder_design.sh
@@ -0,0 +1,45 @@
+# Keona Pang
+# Apr 18, 2025
+
+# Hardware requirements: 4 x A100 CRUSOE instance
+# 4 x GPU, 47 GiB GPU memory
+# 128 GB SSD drive space, 60GiB RAM,24 CPU
+
+#INPUT FILES TO UPLOAD TO JUPYTER NOTEBOOK
+#   - `protein-binder-design_v3.ipynb` uploaded to a [public Github repo](https://github.com/Keonapang/generative-protein-binder-design/blob/main/src/protein-binder-design.ipynb)
+#  - `docker-compose.yaml` (3MB) from the original [BioNeMo repo](https://github.com/NVIDIA-BioNeMo-blueprints/generative-protein-binder-design/blob/main/deploy/docker-compose.yaml)
+#  - `cycle1_alphafold2_output.pdb` (80KB) pre-computed on [AlphaFold2 colab](https://colab.research.google.com/github/sokrypton/ColabFold/blob/main/AlphaFold2.ipynb#scrollTo=R_AH6JSXaeb2)
+
+# EXECUTION
+cycle = 1 # cycle 1 or cycle 2
+num_seq = 4 # number of sequences to generate per target
+diffusion = 30 # number of diffusion steps (15-30 recommended)
+temp = 0.2 # sampling temperature (range: 0-1) to adjust the probability values for the 20 amino acids at each position, controls the diversity of the design outcomes
+
+# Get from brev.nvidia
+brew install brevdev/homebrew-/brev && brev login --token <****> # Install the CLI
+brev shell <instance-name> # find instance-name on brev.nvidias # Open a terminal locally
+
+# Run on Terminal
+sudo apt-get install -y docker-compose
+sudo apt install python3.11
+
+# Log in to Docker
+export NGC_CLI_API_KEY=<enter-key> # enter personal API key
+docker login nvcr.io --username='$oauthtoken' --password="${NGC_CLI_API_KEY}"
+
+## Create the nim cache directory to download any model data to your local/server disk 
+mkdir -p ~/.cache/nim
+chmod -R 777 ~/.cache/nim    ## Make it writable by the NIM
+export HOST_NIM_CACHE=~/.cache/nim
+
+# Run Docker compose
+docker compose 
+
+# check status of NIMs
+curl localhost:8082/v1/health/ready # RFdiffusion
+curl localhost:8083/v1/health/ready # Protein MPNN
+
+pip install requests
+python SCRIPT_protein_binder_design.py --cycle 1 --num_seq 4 --diffusion 30 --temp 0.2
+
--- src/SCRIPT_protein_binder_design.py
+++ src/SCRIPT_protein_binder_design.py
@@ -9,14 +9,33 @@
 
 # INPUT FILES TO UPLOAD TO JUPYTER NOTEBOOK
 #    - `protein-binder-design_v3.ipynb` uploaded to a [public Github repo](https://github.com/Keonapang/generative-protein-binder-design/blob/main/src/protein-binder-design.ipynb)
-#   - `docker-compose.yaml` (3MB) from the original [BioNeMo repo](https://github.com/NVIDIA-BioNeMo-blueprints/generative-protein-binder-design/blob/main/deploy/docker-compose.yaml)
-#   - `cycle1_alphafold2_output.pdb` (80KB) pre-computed on [AlphaFold2 colab](https://colab.research.google.com/github/sokrypton/ColabFold/blob/main/AlphaFold2.ipynb#scrollTo=R_AH6JSXaeb2)
+#    - `docker-compose.yaml` (3MB) from the original [BioNeMo repo](https://github.com/NVIDIA-BioNeMo-blueprints/generative-protein-binder-design/blob/main/deploy/docker-compose.yaml)
+#    - `cycle1_alphafold2_output.pdb` (80KB) pre-computed on [AlphaFold2 colab](https://colab.research.google.com/github/sokrypton/ColabFold/blob/main/AlphaFold2.ipynb#scrollTo=R_AH6JSXaeb2)
 
 # EXECUTION
 # cycle = 1 # cycle 1 or cycle 2
 # num_seq = 4 # number of sequences to generate per target
 # diffusion = 30 # number of diffusion steps (15-30 recommended)
 # temp = 0.2 # sampling temperature (range: 0-1) to adjust the probability values for the 20 amino acids at each position, controls the diversity of the design outcomes
+
+# brew install brevdev/homebrew-/brev && brev login --token <****> # Install the CLI
+# brev shell <instance-name< # find instance-name on brev.nvidias # Open a terminal locally
+
+# update
+# sudo apt-get install -y docker-compose
+# sudo apt install python3.11
+# export NGC_CLI_API_KEY=<enter-key> # enter personal API key
+# docker login nvcr.io --username='$oauthtoken' --password="${NGC_CLI_API_KEY}"
+
+# ## Create the nim cache directory to download any model data to your local/server disk 
+# mkdir -p ~/.cache/nim
+# chmod -R 777 ~/.cache/nim    ## Make it writable by the NIM
+# export HOST_NIM_CACHE=~/.cache/nim
+
+# # Run Docker compose
+# docker compose 
+
+# pip install requests
 # python SCRIPT_protein_binder_design.py --cycle 1 --num_seq 4 --diffusion 30 --temp 0.2
 
 ##########################################################################
@@ -29,10 +48,10 @@ from enum import StrEnum, Enum  # must be Python 3.11+
 from typing import Tuple, Dict, Any, List
 from pathlib import Path
 
+# Load arguments
 parser = argparse.ArgumentParser(description="De Novo Protein Design Workflow")
-parser.add_argument("--cycle", type=int, choices=[1, 2], required=True, help="Cycle number (1 or 2)")
-
-
+parser.add_argument("--cycle", type=str, required=True, help="Cycle number (e.g., '1', '1A', '1B', or '2')")
+parser.add_argument("--contig", type=str, default="20", help="Contig region (e.g., 'A400-600/0 15-25')")
 parser.add_argument("--num_seq", type=int, default=1, help="Number of sequences to generate per target")
 parser.add_argument("--diffusion", type=int, default=20, help="Number of diffusion steps (15-30 recommended)")
 parser.add_argument("--temp", type=float, default=0.2, help="Sampling temperature (range: 0-1)")
@@ -40,28 +59,24 @@ args = parser.parse_args()
 
 # Assign input arguments to variables
 cycle = args.cycle
+contigs = args.contig if args.contig != "20" else contigs
 num_seq = args.num_seq
 diffusion = args.diffusion
 temp = args.temp
 
-# cycle = 1 # cycle 1 or cycle 2
-# num_seq = 4 # number of sequences to generate per target
-# diffusion = 30 # number of diffusion steps (15-30 recommended)
-# temp = 0.2 # sampling temperature (range: 0-1) to adjust the probability values for the 20 amino acids at each position, controls the diversity of the design outcomes
-
-# if cycle == 1, then run code below
-
+# Set up all paths and variables
 root = "/home/ubuntu/nvidia-workbench"
-print(f"Generating {num_seq_per_target} sequence per target...")
+os.makedirs(root, exist_ok=True)
+print(f"Generating {num_seq} sequences per target for cycle {cycle}...")
 
 if num_seq > 1: # if num_seq > 1, then run code below
-    if cycle == 1:
+    if cycle == "1":
         name = f"cycle1_{num_seq}seqs_{diffusion}diff_{temp}temp"
         outdir = f"{root}/cycle1_output"
         contigs="A400-600/0 15-25"
         precomputed_pdb_path = "cycle1_alphafold2_output.pdb" # converts pdb_id to a Path Object;
         target_sequence="MDPPRPALLALLALPALLLLLLAGARAEEEMLENVSLVCPKDATRFKHLRKYTYNYEAESSSGVPGTADSRSATRINCKVELEVPQLCSFILKTSQCTLKEVYGFNPEGKALLKKTKNSEEFAAAMSRYELKLAIPEGKQVFLYPEKDEPTYILNIKRGIISALLVPPETEEAKQVLFLDTVYGNCSTHFTVKTRKGNVATEISTERDLGQCDRFKPIRTGISPLALIKGMTRPLSTLISSSQSCQYTLDAKRKHVAEAICKEQHLFLPFSYKNKYGMVAQVTQTLKLEDTPKINSRFFGEGTKKMGLAFESTKSTSPPKQAEAVLKTLQELKKLTISEQNIQRANLFNKLVTELRGLSDEAVTSLLPQLIEVSSPITLQALVQCGQPQCSTHILQWLKRVHANPLLIDVVTYLVALIPEPSAQQLREIFNMARDQRSRATLYALSHAVNNYHKTNPTGTQELLDIANYLMEQIQDDCTGDEDYTYLILRVIGNMGQTMEQLTPELKSSILKCVQSTKPSLMIQKAAIQALRKMEPKDKDQEVLLQTFLDDASPGDKRLAAYLMLMRSPSQADINKIVQILPWEQNEQVKNFVASHIANILNSEELDIQDLKKLVKEALKESQLPTVMDFRKFSRNYQLYKSVSLPSLDPASAKIEGNLIFDPNNYLPKESMLKTTLTAFGFASADLIEIGLEGKGFEPTLEALFGKQGFFPDSVNKALYWVNGQVPDGVSKVLVDHFGYTKDDKHEQDMVNGIMLSVEKLIKDLKSKEVPEARAYLRILGEELGFASLHDLQLLGKLLLMGARTLQGIPQMIGEVIRKGSKNDFFLHYIFMENAFELPTGAGLQLQISSSGVIAPGAKAGVKLEVANMQAELVAKPSVSVEFVTNMGIIIPDFARSGVQMNTNFFHESGLEAHVALKAGKLKFIIPSPKRPVKLLSGGNTLHLVSTTKTEVIPPLIENRQSWSVCKQVFPGLNYCTSGAYSNASSTDSASYYPLTGDTRLELELRPTGEIEQYSVSATYELQREDRALVDTLKFVTQAEGAKQTEATMTFKYNRQSMTLSSEVQIPDFDVDLGTILRVNDESTEGKTSYRLTLDIQNKKITEVALMGHLSCDTKEERKIKGVISIPRLQAEARSEILAHWSPAKLLLQMDSSATAYGSTVSKRVAWHYDEEKIEFEWNTGTNVDTKKMTSNFPVDLSDYPKSLHMYANRLLDHRVPQTDMTFRHVGSKLIVAMSSWLQKASGSLPYTQTLQDHLNSLKEFNLQNMGLPDFHIPENLFLKSDGRVKYTLNKN"
-    elif cycle == 2:
+    elif cycle == "2":
         name = f"cycle2_{num_seq}seqs_{diffusion}diff_{temp}temp"
         outdir = f"{root}/cycle2_output"
         contigs="A100-300/0 15-25"
@@ -70,24 +85,56 @@ if num_seq > 1: # if num_seq > 1, then run code below
     else:
         raise ValueError("Invalid cycle number. Please set cycle to 1 or 2.")
     
-    cycle = ExampleRequestParams(
-        target_sequence= target_sequence,
-        contigs=contigs,  # Region A400-A600, peptides 15-25 residues long
-        hotspot_res=[], 
-        input_pdb_chains=[], # [Optional] default is to design for all chains in the protein
-        ca_only=False, # [Optional]  CA-only model helps to address specific needs in protein design where focusing on the alpha carbon (CA)
-        use_soluble_model=True, 
-        sampling_temp=[temp], # (range: 0 - 1) adjust the probability values for the 20 amino acids at each position, controls the diversity of the design outcomes
-        diffusion_steps=diffusion, # 15-30 steps are recommended for protein design tasks
-        num_seq_per_target=num_seq  # Generate 4 binders
-    )
-
 if num_seq == 1: # if num_seq > 1, then run code below
-    if cycle == "1A": 
+    outdir = f"{root}/cycle{cycle}_output"
+    name = f"cycle{cycle}_{num_seq}seqs_{diffusion}diff_{temp}temp"
+    
+    if "1" in cycle:
+        target_sequence="MDPPRPALLALLALPALLLLLLAGARAEEEMLENVSLVCPKDATRFKHLRKYTYNYEAESSSGVPGTADSRSATRINCKVELEVPQLCSFILKTSQCTLKEVYGFNPEGKALLKKTKNSEEFAAAMSRYELKLAIPEGKQVFLYPEKDEPTYILNIKRGIISALLVPPETEEAKQVLFLDTVYGNCSTHFTVKTRKGNVATEISTERDLGQCDRFKPIRTGISPLALIKGMTRPLSTLISSSQSCQYTLDAKRKHVAEAICKEQHLFLPFSYKNKYGMVAQVTQTLKLEDTPKINSRFFGEGTKKMGLAFESTKSTSPPKQAEAVLKTLQELKKLTISEQNIQRANLFNKLVTELRGLSDEAVTSLLPQLIEVSSPITLQALVQCGQPQCSTHILQWLKRVHANPLLIDVVTYLVALIPEPSAQQLREIFNMARDQRSRATLYALSHAVNNYHKTNPTGTQELLDIANYLMEQIQDDCTGDEDYTYLILRVIGNMGQTMEQLTPELKSSILKCVQSTKPSLMIQKAAIQALRKMEPKDKDQEVLLQTFLDDASPGDKRLAAYLMLMRSPSQADINKIVQILPWEQNEQVKNFVASHIANILNSEELDIQDLKKLVKEALKESQLPTVMDFRKFSRNYQLYKSVSLPSLDPASAKIEGNLIFDPNNYLPKESMLKTTLTAFGFASADLIEIGLEGKGFEPTLEALFGKQGFFPDSVNKALYWVNGQVPDGVSKVLVDHFGYTKDDKHEQDMVNGIMLSVEKLIKDLKSKEVPEARAYLRILGEELGFASLHDLQLLGKLLLMGARTLQGIPQMIGEVIRKGSKNDFFLHYIFMENAFELPTGAGLQLQISSSGVIAPGAKAGVKLEVANMQAELVAKPSVSVEFVTNMGIIIPDFARSGVQMNTNFFHESGLEAHVALKAGKLKFIIPSPKRPVKLLSGGNTLHLVSTTKTEVIPPLIENRQSWSVCKQVFPGLNYCTSGAYSNASSTDSASYYPLTGDTRLELELRPTGEIEQYSVSATYELQREDRALVDTLKFVTQAEGAKQTEATMTFKYNRQSMTLSSEVQIPDFDVDLGTILRVNDESTEGKTSYRLTLDIQNKKITEVALMGHLSCDTKEERKIKGVISIPRLQAEARSEILAHWSPAKLLLQMDSSATAYGSTVSKRVAWHYDEEKIEFEWNTGTNVDTKKMTSNFPVDLSDYPKSLHMYANRLLDHRVPQTDMTFRHVGSKLIVAMSSWLQKASGSLPYTQTLQDHLNSLKEFNLQNMGLPDFHIPENLFLKSDGRVKYTLNKN",
+        precomputed_pdb_path = "cycle1_alphafold2_output.pdb" # 
 
+        if cycle == "1A": 
+            contigs="A400-440/0 15-25"
+        if cycle == "1B":
+            contigs="A450-490/0 15-25"
+        if cycle == "1C":
+            contigs="A500-540/0 15-25"
+        if cycle == "1D":
+            contigs="A550-590/0 15-25"
+    
+    if "2" in cycle:
+        target_sequence="GKIDFLNNYALFLSPSAQQASWQVSARFNQYKYNQNFSAGNNENIMEAHVGINGEANLDFLNIPLTIPEMRLPYTIITTPPLKDFSLWEKTGLKEFLKTTKQSFDLSVKAQYKKNKHRHSITNPLAVLCEFISQSIKSFDRHFEKNRNNALDFVTKSYNETKIKFDKYKAEKSHDELPRTFQIPGYTVPVVNVEVSPFTIEMSAFGYVFPKAVSMPSFSILGSDVRVPSYTLILPSLELPVLHVPRNLKLSLPDFKELCTISHIFIPAMGNITYDFSFKSSVITLNTNAELFNQSDIVAHLLSSSSSVIDALQYKLEGTTRLTRKRGLKLATALSLSNKFVEGSHNSTVSLTTKNMEVSVATTTKAQIPILRMNFKQELNGNTKSKPTVSSSMEFKYDFNSSMLYSTAKGAVDHKLSLESLTSYFSIESSTKGDVKGSVLSREYSGTIASEANTYLNSKSTRSSVKLQGTSKIDDIWNLEVKENFAGEATLQRIYSLWEHSTKNHLQLEGLFFTNGEHTSKATLELSPWQMSALVQVHASQPSSFHDFPDLGQEVALNANTKNQKIRWKNEVRIHSGSFQSQVELSNDQEKAHLDIAGSLEGHLRFLKNIILPVYDKSLWDFLKLDVTTSIGRRQHLRVSTAFVYTKNPNGYSFSIPVKVLADKFIIPGLKLNDLNSVLVMPTFHVPFTDLQVPSCKLDFREIQIYKKLRTSSFALNLPTLPEVKFPEVDVLTKYSQPEDSLIPFFEITVPESQLTVSQFTLPKSVSDGIAALDLNAVANKIADFELPTIIVPEQTIEIPSIKFSVPAGIVIPSFQALTARFEVDSPVYNATWSASLKNKADYVETVLDSTCSSTVQFLEYELNVLGTHKIEDGTLASKTKGTFAHRDFSAEYEEDGKYEGLQEWEGKAHLNIKSPAFTDLHLRYQKDKKGISTSAASPAVGTVGMDMDEDDDFSKWNFYYSPQSSPDKKLTIFKTELRVRESDEETQIKVNWEEEAASGLLTSLKDNVPKATGVLYDYVNKYHWEHTGLTLREVSSKLRRNLQNNAEWVYQGAIRQIDDIDVRFQKAASGTTGTYQEWKDKAQNLYQELLTQEGQASFQGLKDNVFDGLVRVTQEFHMKVKHLIDSLIDFLNFPRFQFPGKPGIYTREELCTMFIREVGTVLSQVYSKVHNGSEILFSYFQDLVITLPFELRKHKLIDV",
+        precomputed_pdb_path = "cycle2_alphafold2_output.pdb" # 
 
+        if cycle == "2A":
+            contigs="A100-140/0 15-25"
+        if cycle == "2B":
+            contigs="A150-190/0 15-25"
+        if cycle == "2C":
+            contigs="A200-240/0 15-25"
+        if cycle == "2D":
+            contigs="A250-290/0 15-25"
+    else:
+        raise ValueError("Invalid cycle number.")
+
+cycle = ExampleRequestParams(
+    target_sequence= target_sequence,
+    contigs=contigs,  # Region A400-A600, peptides 15-25 residues long
+    hotspot_res=[], 
+    input_pdb_chains=[], # [Optional] default is to design for all chains in the protein
+    ca_only=False, # [Optional]  CA-only model helps to address specific needs in protein design where focusing on the alpha carbon (CA)
+    use_soluble_model=True, 
+    sampling_temp=[temp], # (range: 0 - 1) adjust the probability values for the 20 amino acids at each position, controls the diversity of the design outcomes
+    diffusion_steps=diffusion, # 15-30 steps are recommended for protein design tasks
+    num_seq_per_target=num_seq  # Generate 4 binders
+)
+example=cycle
 os.makedirs(outdir, exist_ok=True)
+print(f"Output directory: {outdir}")
 
+# check if  precomputed_pdb_path exists in the current directory
+if not os.path.exists(precomputed_pdb_path):
+    raise FileNotFoundError(f"Precomputed PDB file {precomputed_pdb_path} does not exist. Please check the path.")
 
 ##############################################################
 # SET UP 
@@ -189,11 +236,9 @@ print(f"ProteinMPNN ready: {status}")
 ##############################################################
 # 2. RFdiffusion
 ##############################################################
-
-example=cycle
+precomputed_pdb=get_reduced_pdb(precomputed_pdb_path, rcsb_path=None)
 
 print(f"Running RFdiffusion....")
-precomputed_pdb=get_reduced_pdb(precomputed_pdb_path, rcsb_path=None)
 rfdiffusion_query = {
     "input_pdb": precomputed_pdb,  # Now using the precomputed PDB structure
     "contigs": example.contigs,
@@ -205,7 +250,8 @@ rc, rfdiffusion_response = query_nim(
     nim_endpoint=NIM_ENDPOINTS.RFDIFFUSION.value,
     nim_port=NIM_PORTS.RFDIFFUSION_PORT.value
 )
-## Print the first 160 characters of the RFDiffusion PDB output
+
+# save
 print(rfdiffusion_response["output_pdb"][0:160])
 with open(f"{root}/2_rfdiffusion_{name}.pdb", "w") as pdb_file:
     pdb_file.write(rfdiffusion_response["output_pdb"])
@@ -216,19 +262,18 @@ with open(f"{root}/2_rfdiffusion_{name}.pdb", "w") as pdb_file:
 
 print(f"Running ProteinMPNN....")
 proteinmpnn_query = {
-        "input_pdb" : rfdiffusion_response["output_pdb"],
-        "input_pdb_chains" : example.input_pdb_chains,
-        "ca_only" : example.ca_only,
-        "use_soluble_model" : example.use_soluble_model,
-        "num_seq_per_target" : example.num_seq_per_target,
-        "sampling_temp" : example.sampling_temp
+    "input_pdb" : rfdiffusion_response["output_pdb"],
+    "input_pdb_chains" : example.input_pdb_chains,
+    "ca_only" : example.ca_only,
+    "use_soluble_model" : example.use_soluble_model,
+    "num_seq_per_target" : example.num_seq_per_target,
+    "sampling_temp" : example.sampling_temp
 }
 rc, proteinmpnn_response = query_nim(
     payload=proteinmpnn_query,
     nim_endpoint=NIM_ENDPOINTS.PROTEINMPNN.value,
     nim_port=NIM_PORTS.PROTEINMPNN_PORT.value
 )
-
 fasta_sequences = [x.strip() for x in proteinmpnn_response["mfasta"].split("\n") if '>' not in x][2:]
 binder_target_pairs = [[binder, example.target_sequence] for binder in fasta_sequences]
 
@@ -243,15 +288,13 @@ with open(f"{root}/3_proteinmpnn_target_{name}.txt", "w") as txt_file:
 with open(f"{root}/3_proteinmpnn_{name}.fasta", "w") as fasta_file:
     fasta_file.write(proteinmpnn_response["mfasta"])
 
-# Extract scores/probs array
-scores = proteinmpnn_response["scores"]
-probs = proteinmpnn_response["probs"]
-
 # Save scores and probs to files
+scores = proteinmpnn_response["scores"]
 with open(f"{root}/3_proteinmpnn_scores_{name}.txt", "w") as scores_file:
     for i, score in enumerate(scores):
         scores_file.write(f"Sequence {i+1}: Score = {score}\n")
 
+probs = proteinmpnn_response["probs"]
 with open(f"{root}/3_proteinmpnn_probs_{name}.txt", "w") as probs_file:
     for i, prob_matrix in enumerate(probs):
         probs_file.write(f"Sequence {i+1}:\n")
