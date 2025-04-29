55530995a3346aa06df9a97be314ddc093bec6ec
Keonapang <keonapang2000@gmail.com>
2025-04-29 11:06:20 -0400

    write Apob-100 peptide binder design script

--- src/RUN_protein_binder_design.sh
+++ src/RUN_protein_binder_design.sh
@@ -45,14 +45,11 @@ done
 for cycle in "1A" "1B" "1C" "1D" "2A" "2B" "2C" "2D"; do
     python3.11 /home/ubuntu/3_protein_binder_design.py --cycle "$cycle" --num_seq 1 --diffusion 50 --temp 0.5
 done
-for cycle in "1DE" "1E"; do
+for cycle in "1AA" "1BB" "1CC" "1DD"; do
     python3.11 /home/ubuntu/3_protein_binder_design.py --cycle "$cycle" --num_seq 1 --diffusion 50 --temp 0.5
 done
 # ----------------------------------------------------
 
-for cycle in "1" "2"; do
-    python3.11 /home/ubuntu/3_protein_binder_design.py --cycle "$cycle" --num_seq 4 --diffusion 50 --temp 0.4
-done
 
 # python3.11 3_protein_binder_design.py --cycle "$cycle" --num_seq 4 --diffusion 25 --temp 0.4
 # python3.11 3_protein_binder_design.py --cycle "$cycle" --num_seq 5 --diffusion 25 --temp 0.4
